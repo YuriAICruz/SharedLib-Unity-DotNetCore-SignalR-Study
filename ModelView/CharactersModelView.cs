b40a8628c83daecbf98489a5f13ee75c8815c9be
YuriAICruz <yuriaicruz@gmail.com>
2020-02-03 19:52:50 -0300

    characters model

---
+++ ModelView/CharactersModelView.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+#if UNITY_2018_3_OR_NEWER
+using UnityEngine;
+#endif
+
+namespace Graphene.SharedModels.ModelView
+{
+    public class CharactersModelView
+    {
+        [Required] public int Id;
+        [Required] public string Name;
+
+        public float[] Color;
+
+
+#if UNITY_2018_3_OR_NEWER
+        public Color GetColor()
+        {
+            return new Color(Color[0], Color[1], Color[2], Color[3]);
+        }
+#endif
+    }
+}
